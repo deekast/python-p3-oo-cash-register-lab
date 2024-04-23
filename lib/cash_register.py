db1758d1a576fb6567c93b3d2e4e5ec3aa4279d3
deekast <96024516+deekast@users.noreply.github.com>
2024-04-23 16:22:07 -0400

    class lab push

--- lib/cash_register.py
+++ lib/cash_register.py
@@ -1,4 +1,30 @@
 #!/usr/bin/env python3
 
 class CashRegister:
-  pass
+
+  def __init__(self, discount=0):
+    self.discount = discount
+    self.total = 0
+    self.items = []
+  
+  def add_item(self, title, price, quantity = 1):
+    self.total += (price * quantity)
+    self.last_transaction = {
+      "title": title,
+      'price': price,
+      'quantity': quantity
+
+    }
+    for _ in range(quantity):
+      self.items.append(title)
+
+  def apply_discount(self):
+    if self.discount == 0:
+      print ("There is no discount to apply.")
+    else:
+      self.total = self.total - (self.total * (self.discount / 100))
+      print(f"After the discount, the total comes to ${int(self.total)}.")
+
+  def void_last_transaction(self):
+    for _ in range(self.last_transaction.get('quantity')):
+      self.total -= self.last_transaction.get('price')
